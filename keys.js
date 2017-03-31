044d5d0a7ef6207157a0d812153c9679eda3378f
MLangteau <LangteauM@gmail.com>
2017-03-31 19:08:34 -0400

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'pIBYrPfZfJK0UUp4J0WjEvfDT',
+  consumer_secret: 'AsF1z5ShkBd2uuSkMCs4bRsp7m4BqRmYDMiLH1raGlys7jgEcU',
+  access_token_key: '847575147611475968-yIs5FGEjUxrCcHFqyzA92OkWoVYG8pw',
+  access_token_secret: 'fl4QOIBcLTEK0h2kgPwP5QIW1Fuc4V4NUlAirDtlVoVbj',
 }
