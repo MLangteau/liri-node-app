29f28c3b77d03cf35d9656e1f50bc3849b57908e
MLangteau <langteauM@gmail.com>
2017-04-02 12:42:37 -0400

    tweets done

--- liri.js
+++ liri.js
@@ -14,22 +14,27 @@ var value = process.argv[3];
 
 switch (action) {
   case "help":
+  case "h":
     console.log("type one of the following commands:\n node my-tweets \n node spotify-this-song '<song name here>' \n node movie-this '<movie name here>'  \n node do-what-it-says");
     break;
 
   case "my-tweets":
+  case "tw":
     tweet();
     break;
 
   case "spotify-this-song":
+  case "sp":
     spot();
     break;
 
   case "movie-this":
+  case "mo":
     movies();
     break;
 
   case "do-what-it-says":
+  case "do":
     doWhat();
     break;
 }
@@ -38,16 +43,29 @@ function tweet() {
 	
   console.log("in tweet function");
   var client = new Twitter(keys.twitterKeys);
-//  var params = {screen_name: 'ReenieSwim'};
-  var params = {screen_name: 'wsj'};
+  var params = {screen_name: 'ReenieSwim'};
+//  var params = {screen_name:'flotus'}; 
 
   client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
-    console.log(tweets);
+    console.log("tweets.length: " + tweets.length);
+    if (tweets.length > 20) {
+      newTweetslength = 20;
+    }
+    else {
+      newTweetslength = tweets.length;  
+    }
+    console.log("These are in Latest tweets first order: ");
+    for (var i=0; i<tweets.length; i++){
+      console.log(tweets[i].created_at + " " + tweets[i].text + "\n");
+    }
   }
-});
+  else {
+    console.log("error is finding tweet!!!!!!!!!!!!")
+  }
+  });  // end of client.get
 
-};
+}; // end of tweet function
 
 function spot() {
 
@@ -55,12 +73,14 @@ function spot() {
 
   spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
     if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
+      console.log('Error occurred: ' + err);
+      return;
     }
- 
+    else {
     // Do something with 'data'
-  
+      console.log("Spotify data: ",data);
+
+    }
   }); // end of spotify search function
 }  // end of spot function
 function movies() {
